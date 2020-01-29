ba768e96275921ac8b0950a64dad3a2262578bf3
henry <henry.huang0710@gmail.com>
2020-01-29 22:30:13 +1100

    heroku

---
+++ client/loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+    int count = 0;
+    while (count <= 5) {
+
+        printf("%d ", count);
+        count++;
+    }
+
+    printf("\n");
+    return 0;
+}
